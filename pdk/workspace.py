79726c14a827450893c9fa92c66ed6b41ac77297
Free Ekanayaka <free@64studio.com>
2008-09-24 19:43:29 +0100

    Removed smartpm hack, bug reported upstream, using smartpm << 0.51 from now on

--- pdk/workspace.py
+++ pdk/workspace.py
@@ -970,10 +970,6 @@ def run_closure(component_names, arch, outfile):
                 if loader.getChannel().getType() == 'pdk-deb':
                     need=0
             if need == 1:
-                # HACK: smartpm insists in wanting this
-                if package.name == 'debconf-english':
-                    continue
-
                 # scary assumption
                 loader = package.loaders.keys()[0]
                 missing += 1
